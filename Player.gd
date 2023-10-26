3f413fbc3bd9ed6da81d822bd48ee271c455076b
Ryan Beckett <rabeckett716@gmail.com>
2023-10-26 12:30:46 -0400

    animation update

--- Player.gd
+++ Player.gd
@@ -1,22 +1,46 @@
 extends CharacterBody2D
 
+enum DirectionStates {Up, Down, Left, Right}
+enum ActionStates {Idle, Run, Attack}
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var CurrentMoveState : int
+var CurrentDirection : int
+
+@export var TopSpeed = 300.0
+var CurrentSpeed = 0
+var direction
+
+func _ready():
+	CurrentMoveState = ActionStates.Idle
+	CurrentDirection = DirectionStates.Down
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction = Input.get_vector("Run_Left", "Run_Right", "Run_Up", "Run_Down")
-	print(direction)
+	direction = Input.get_vector("Run_Left", "Run_Right", "Run_Up", "Run_Down")
 	if direction != Vector2(0,0):
-		velocity = direction * SPEED
+		CurrentSpeed = lerpf(CurrentSpeed, TopSpeed, 1.75 * delta)
+		velocity = direction * CurrentSpeed
+		CurrentMoveState = ActionStates.Run
 	else:
 		velocity = Vector2(0, 0)
+		CurrentSpeed = 0
+		CurrentMoveState = ActionStates.Idle
 
 	move_and_slide()
+	GetSpriteDirection()
 	
+	if Input.is_action_just_pressed("Attack"):
+		Attack()
+		
+func GetSpriteDirection():
 	match direction:
-		Vector2(0, 0):
-			$AnimatedSprite2D.animation
+		Vector2.DOWN:
+			CurrentDirection = DirectionStates.Down
+		Vector2.UP:
+			CurrentDirection = DirectionStates.Up
+		Vector2.RIGHT:
+			CurrentDirection = DirectionStates.Right
+		Vector2.LEFT:
+			CurrentDirection = DirectionStates.Left
+
+func Attack():
+	CurrentMoveState = ActionStates.Attack
