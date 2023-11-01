6948f2c6c8aba77acac82317fd1dea14c9afa304
Ryan Beckett <rabeckett716@gmail.com>
2023-11-01 13:01:49 -0400

    ui update

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,14 +1,20 @@
 extends Control
 
 @onready var TitleBox = $"Title Container"
+@onready var ButtonBox = $"Button Container"
 
 @onready var TitleLetters = TitleBox.get_children()
+@onready var ButtonList = ButtonBox.get_children()
 
 func _ready():
 	for i in range(TitleLetters.size()):
 		TitleLetters[i].label_settings.set_font_color(Color.BLACK)
 	
-	await get_tree().create_timer(3).timeout
+	for i in range(ButtonList.size()):
+		ButtonList[i].visible = false
+		ButtonList[i].mouse_filter = MOUSE_FILTER_IGNORE
+	
+	await get_tree().create_timer(1.5).timeout
 	TitleFX()
 
 func _process(delta):
@@ -19,3 +25,16 @@ func TitleFX():
 		var colorTween = get_tree().create_tween()
 		colorTween.tween_property(TitleLetters[i].label_settings, "font_color", Color.WHITE, 0.5)
 		await get_tree().create_timer(0.15).timeout
+		
+	for i in range(ButtonList.size()):
+		ButtonList[i].visible = true
+		ButtonList[i].mouse_filter = MOUSE_FILTER_STOP
+		
+		await get_tree().create_timer(0.25).timeout
+
+func NewGame():
+	print("Starting new game...")
+	get_tree().change_scene_to_file("res://Game Management/GameManager.tscn")
+	
+func QuitGame():
+	get_tree().quit()
