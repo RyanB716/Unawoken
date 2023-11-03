1654488b74d1936ec97b732f8430ced715064efb
Ryan Beckett <rabeckett716@gmail.com>
2023-11-03 12:29:55 -0400

    init

---
+++ Scripts/Enemy Management/BaseEnemyClass.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+class_name BasicEnemy
+
+@export_category("Combat Stats")
+@export var MaxHealth : int
+@export var NumberOfAttacks : int
+@export var DamageMinimum : int
+@export var DamageMaximum : int
+
+@export_category("Movement Stats")
+@export var BaseSpeed : float
+@export var TopSpeed : float
+
+@export_category("Surveilance Stats")
+@export var DetectionRange : float
+
+func _physics_process(delta):
+	move_and_slide()
