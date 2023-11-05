d783e8226a5553fe30037ca6441906afdf8dd2d3
Ryan Beckett <rabeckett716@gmail.com>
2023-11-05 18:35:43 -0500

    update

--- Scripts/Enemy Management/EnemyStates/Enemy_Attack.gd
+++ Scripts/Enemy Management/EnemyStates/Enemy_Attack.gd
@@ -7,6 +7,5 @@ func OnEnter():
 	print("Attacking")
 	SelfRef.velocity = Vector2.ZERO
 	SelfRef.AnimPlayer.play("Skeleton_Attack")
-	SelfRef.AttackSFX.play()
 	await SelfRef.AnimPlayer.animation_finished
 	Transitioned.emit("Idle")
