bb0a62ea1fea93685a4931af8bd6478268412dfc
Ryan Beckett <rabeckett716@gmail.com>
2023-11-06 10:35:26 -0500

    various updates

--- Scripts/Enemy Management/EnemyStates/Enemy_Attack.gd
+++ Scripts/Enemy Management/EnemyStates/Enemy_Attack.gd
@@ -8,4 +8,12 @@ func OnEnter():
 	SelfRef.velocity = Vector2.ZERO
 	SelfRef.AnimPlayer.play("Skeleton_Attack")
 	await SelfRef.AnimPlayer.animation_finished
-	Transitioned.emit("Idle")
+	
+	if SelfRef.PlayerTarget != null:
+		var Direction = SelfRef.PlayerTarget.global_position - SelfRef.global_position
+	
+		if Direction.length() < SelfRef.DetectionRange:
+			Transitioned.emit("Follow")
+		else:
+			Transitioned.emit("Idle")
+	
--- Scripts/Enemy Management/EnemyStates/Enemy_Follow.gd
+++ Scripts/Enemy Management/EnemyStates/Enemy_Follow.gd
@@ -3,17 +3,30 @@ class_name Enemy_Follow
 
 @export var SelfRef : BasicEnemy
 
+var Direction
+
 func OnEnter():
 	print("Skeleton: FOLLOWING")
 	
 func PhysicsUpdate(_deta : float):
 	if SelfRef.PlayerTarget != null:
-		var Direction = SelfRef.PlayerTarget.global_position - SelfRef.global_position
+		Direction = SelfRef.PlayerTarget.global_position - SelfRef.global_position
 	
 		if Direction.length() > SelfRef.AttackRange:
 			SelfRef.velocity = Direction.normalized() * SelfRef.TopSpeed
 		else:
 			Transitioned.emit("Attack")
 			
+		if Direction.length() > SelfRef.DisengagementRange:
+			Transitioned.emit("Idle")
+
+func DecideToAttack():
+	var RNG = RandomNumberGenerator.new()
+	
+	var newValue = RNG.randi_range(0, 100)
+	
+	if newValue >= 75:
+		Transitioned.emit("Attack")
+	else:
 		if Direction.length() > SelfRef.DisengagementRange:
 			Transitioned.emit("Idle")
