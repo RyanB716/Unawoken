0df2e8eec53414aef13c7ec918afa9e68ffb7cb9
Ryan Beckett <rabeckett716@gmail.com>
2023-11-07 16:34:39 -0500

    sfx update

--- Scripts/Components/HitBox.gd
+++ Scripts/Components/HitBox.gd
@@ -3,9 +3,20 @@ class_name HitBox
 
 @export var TargetReference : CharacterBody2D
 
+@export var ImpactSFX  = []
+
 func _ready():
 	if self.get_child(0).disabled == true:
 		self.get_child(0).disabled = false
 
 func TakeDamage(Amnt : int):
 	TargetReference.CurrentHealth -= Amnt
+	
+	var RNG = RandomNumberGenerator.new()
+	var index = RNG.randi_range(0, ImpactSFX.size() - 1)
+	var NewPlayer = AudioStreamPlayer.new()
+	add_child(NewPlayer)
+	NewPlayer.stream = ImpactSFX[index]
+	NewPlayer.play()
+	await NewPlayer.finished
+	NewPlayer.queue_free()
