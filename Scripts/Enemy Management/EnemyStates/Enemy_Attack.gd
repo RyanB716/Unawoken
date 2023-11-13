319cbf8eac3191955bad214a47c4d48cfae5f23d
Ryan Beckett <rabeckett716@gmail.com>
2023-11-13 16:14:42 -0500

    update

--- Scripts/Enemy Management/EnemyStates/Enemy_Attack.gd
+++ Scripts/Enemy Management/EnemyStates/Enemy_Attack.gd
@@ -9,24 +9,13 @@ func OnEnter():
 	SelfRef.velocity = Vector2.ZERO
 	
 	Direction = SelfRef.PlayerTarget.global_position - SelfRef.global_position
-	var NormDir = Direction.normalized()
-	print(NormDir)
-	if NormDir.x < NormDir.y:
-		if NormDir.y >= 0.1:
-			print("Attacking Down")
-			SelfRef.AnimPlayer.play("Attack_D")
-		else:
-			print("Attacking Up")
-			SelfRef.AnimPlayer.play("Attack_U")
+	print(Direction)
+	if Direction.x > Direction.y:
+		print("Attacking Horrizontally")
 	else:
-		if NormDir.x <= 0.1:
-			print("Attacking Left")
-			SelfRef.AnimPlayer.play("Attack_L")
-		else:
-			print("Attacking Right")
-			SelfRef.AnimPlayer.play("Attack_R")
+		print("Attacking Vertically")
 		
-	await SelfRef.AnimPlayer.animation_finished
+	#await SelfRef.AnimPlayer.animation_finished
 	
 	if SelfRef.CurrentHealth >= 1:
 		if Direction.length() <= SelfRef.DetectionRange:
